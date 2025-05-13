837da301ce78840d478a34e90a33fd91f2433834
Júlia Pereira <jutella31@gmail.com>
2025-05-13 09:07:23 -0300

    .

---
+++ Quiz/script.js
@@ -0,0 +1,105 @@
+//CADA PERGUNTA TEM:
+// - A pergunta
+// - as opçoes
+// - a alternativa certa (começa no 0)
+const perguntas = [
+    {
+        pergunta: "Qual é o nome da nossa escola?",
+        opcoes: ["Luis Eulalio de Bueno Vidigal Filho", "Luis Eulalio", "Luis de Bueno", "Luis Eulalio de Vidigal"],
+            correta: 0
+    },
+    {
+        pergunta: "Qual o nome do nosso curso?",
+        opcoes: ["Analise e Desenvolvimento de Sistemas", "Sistemas de informação", "Desenvolvimento de sistemas", "Ciência da Computação"],
+        correta: 2
+    },
+    {
+        pergunta: "Em que ano foi fundada a nossa escola?",
+        opcoes: ["1999","1942","1985","1970"],
+        correta: 1
+    }
+]
+// variaveis para controlar o quiz
+let perguntaAtual = 0 //Qual pergunta esta senod mostrada
+let pontuacao = 0 //quantidade de acertos
+let erros = 0 //qauntidade de erros
+let opcaoSelecionada = null //qual opcao o usuario selecionou
+
+const pergunta = document.getElementById("pergunta")
+const opcoes = document.getElementById("opcoes")
+const botaoProxima = document.getElementById("proxima")
+const quiz = document.getElementById("quiz")
+const pontuacaoFinal = document.getElementById("pontuacao")
+const valorPontuacao = document. getElementById("valor-pontuacao")
+const botaoReiniciar = document.getElementById("reiniciar")
+const errosContador = document.getElementById ("erros")
+const acertos = document.getElementById("acertos")
+
+// função que atualiza o placar
+function atualizarPlacar() {
+    acertos.textContent = pontuacao
+    errosContador.textContent = erros
+}
+//função que mostra a pergunta atual
+function mostrarPergunta() {
+    // pega a pergunta atual
+    const perguntaAtualObj = perguntas[perguntaAtual] 
+    //mostra o texto da pergunta
+    pergunta.textContent = perguntaAtualObj.pergunta
+    opcoes.textContent = "" //limpa as opcoes anteriores
+    //cria um abotão para cada opção de resposta
+    perguntaAtualObj.opcoes.forEach((opcao, indice) => {
+        const botao = document. createElement ("button")
+        botao. textContent = opcao
+        botao. classList.add("opcao")
+        botao. addEventListener("click", () => selecionarOpcao(indice))
+        opcoes. appendChild(botao)
+    })
+    opcaoSelecionada = null
+    botaoProxima.disabled = true
+}
+//para quando o usuario escolher uma opção
+function selecionarOpcao(indice) {
+    opcaoSelecionada = indice
+    const opcoes = document.querySelectorAll(".opcao")
+    opcoes.forEach((opcao, i) => {
+        opcao.classList.toggle("selecionada", i == indice)
+    })
+    botaoProxima.disabled = false //habilita o botão de proxima
+}
+function mostrarPontuacao() {
+    quiz.classList.add("esconder")
+    pontuacaoFinal.classList.remove("esconder")
+    valorPontuacao.textContent = pontuacao
+}
+//funcao para ir para a proxima pergunta
+botaoProxima.addEventListener("click", () => {
+    if(opcaoSelecionada == perguntas[perguntaAtual].correta) {
+        pontuacao++
+    } else {
+        erros++
+    }
+    atualizarPlacar()
+
+    perguntaAtual++
+    if(perguntaAtual < perguntas.length) {
+        mostrarPergunta()
+    } else {
+        mostrarPontuacao()
+    }
+})
+//botao para reinciar o quiz
+botaoReiniciar.addEventListener("click", () => {
+    //reseta todas as variaveis
+    perguntaAtual = 0
+    pontuacao = 0
+    erros = 0
+    //o quiz aparece e a pontuacao final some
+    quiz.classList.remove("esconder")
+    pontuacaoFinal.classList.add("esconder")
+
+    atualizarPlacar()
+    mostrarPergunta()
+})
+//inicia o quiz com a primeira pergunta
+mostrarPergunta()
